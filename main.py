65c743708c1366d94dedad39a8bc3ad8330ad164
Алёна Остапенко <lexa.trukry319@gmail.com>
2026-01-30 13:25:13 +0300

    2 задание

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-# 1 задание с функцией get_sum
-def get_sum(a, b):
-    return a + b
-print(get_sum(1, 2))
-print(get_sum(2, 2))
-print(get_sum(-5, 10))
+# 2 задание с функцией count_capital_letters
+def count_capital_letters(text):
+    count = 0
+    for char in text:
+        if char.isupper():
+            count += 1
+    return count
+
+print(count_capital_letters("Hello World"))
+print(count_capital_letters("   A   "))
+print(count_capital_letters("hello world"))
+print(count_capital_letters("PYTHON"))
